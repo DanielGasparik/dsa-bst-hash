e0a50a9a9a5bdcdb84dedb8cacf20ee78e23f289
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-03-12 19:32:43 +0100

    added recursive insertion

--- avl.js
+++ avl.js
@@ -1,4 +1,7 @@
-class Node{
+//swap
+let a,b;
+[a,b] = [b,a];
+class AVLNode{
     constructor(value){
         this.value = value;
         this.right = null;
@@ -7,29 +10,68 @@ class Node{
     }
 }
 
-
-class Tree{
-    constructor(value){
+class AVLTree{
+    constructor(){
         this.base = null;
     
     }
     height(n){ 
-        if(n==null){return -1}
+        if(n==null){return -1;}
         return Math.max(height(n.left),height(n.right))+1;
     }
-    balanced(n){
+    balance(n){
         return n==null?0:this.height(n.left)-this.height(n.right);
 
     }
     insert(value){
-        const node = new Node(value);
+        const node = new AVLNode(value);
+        let root = this.base;
         if(this.base == null){
             this.base = node;
-            return this;
-        }        
+            
+        }
+        else{
+            insertN(this.base,node);
+        }
+          
+               
 
+    }
+    insertN(root,node){
+        if(root === null){
+            root = node;
+        }
+        else if(node.value < root.value){
+            root.left = insertN(root.left,node);
+            
+        }
+        else if(node.value>root.value){
+            
+            root.right = insertN(root.right,node);
+        }
+        return root;
+}
+    inorder(node){
+        
+        if(node!==null){
+            this.inorder(node.left);
+            console.log(node.value);
+            this.inorder(node.right);
+        }
+
+    }
+    getRoot(){
+        return this.base;
     }
 
 
     
 }
+console.log("hello world");
+const tree = new AVLTree();
+tree.insert(10);
+tree.insert(20);
+tree.insert(12);
+tree.insert(40);
+tree.insert(12);
+tree.inorder(tree.base);
