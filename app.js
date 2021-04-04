1d93a22c53c5c6b1f42c9b851fef0f7e88d949bd
Daniel Gasparik <daniel.gasparik1@gmail.com>
2021-04-04 13:05:02 +0200

    added testing file

--- app.js
+++ app.js
@@ -1,5 +1,88 @@
 
-console.log("hello world");
-const tree = new avl.AVLTree();
-tree.insert(10);
-console.log(tree.value);
+///TEST FILE;
+/*
+let ht = new HashTable();
+
+
+
+const start = Date.now()
+
+
+
+console.log(ht.search("Anthony Wilson"));
+*/
+
+const fs = require('fs');
+const RedBlackTree = require('functional-red-black-tree')
+const h = require('./hash.js')
+const a = require('./avl.js')
+//moja implementacia
+const ht = new h.HashTable();
+const avl = new a.AVLTree();
+
+//prevzata
+const rbtree = RedBlackTree();
+const ht_p = new Map();
+
+
+
+let data_avl = fs.readFileSync("ExportJson-5.json", "utf8");
+let data_inner_avl = JSON.parse(data_avl);
+
+let data_hash = fs.readFileSync("ExportJson-4.json", "utf8")
+let data_inner_hash = JSON.parse(data_hash);
+
+//Testing AVL
+console.log("Testing my AVL tree implementation on 20k {name:\"name\",age:49} objects");
+const avl_start = Date.now();
+console.log("Inserting.....")
+for (let i of data_inner_avl.items) {
+    avl.insert(i.meno,i);
+}
+const avl_result = Date.now() - avl_start;
+console.log(`This took me ${avl_result}ms. `);
+console.log("Vyhladavam.....")
+const avl_find_start = Date.now();
+console.log(avl.find("David Irving"));
+const avl_find_result = Date.now()-avl_find_start;
+console.log(`This took me ${avl_find_result}ms. `);
+
+//Testing RBT
+/*
+const rbt_insert_start = Date.now();
+for (let i of data_inner_hash.items) {
+    rbtree.insert(i.meno,i);
+}
+console.log(Date.now()-rbt_insert_start+"ms");
+*/
+/*
+//Testing HashTable
+console.log("Testing my HashTable implementation\n")
+console.log("Inserting.....")
+const ht_start = Date.now();
+for (let i of data_inner_hash.items) {
+    ht.insert(i);
+}
+const ht_result = Date.now() - ht_start;
+console.log(`This took me ${ht_result}ms. `);
+console.log("Vyhladavam.....")
+const ht_find_start = Date.now();
+console.log(ht.search("Anais Weatcroft"));
+const ht_find_result = Date.now()-ht_find_start;
+console.log(`This took me ${ht_find_result}ms. `);
+console.log("\n\n\n")
+//Testing HashTable in JS Map
+console.log("Testing native JS Map() (kinda same with hashtable but the implementation varies with different engines (Node, V8-chrome etc)) implementation\n");
+console.log("Inserting.....")
+const htp_start = Date.now();
+for (let i of data_inner_hash.items) {
+    ht_p.set(i.meno,i);
+}
+const htp_result = Date.now() - htp_start;
+console.log(`This took me ${htp_result}ms. `);
+console.log("Vyhladavam.....")
+const htp_find_start = Date.now();
+console.log(ht_p.get("Anais Weatcroft"));
+const htp_find_result = Date.now()-htp_find_start;
+console.log(`This took me ${htp_find_result}ms. `);
+*/
